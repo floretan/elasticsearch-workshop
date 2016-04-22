a9d41c756152ca4a316b25ce9ed96fae82379e7e
Florian Lorétan <florian.loretan@wunderkraut.com>
2016-04-22 17:16:23 +0200

    Add javascript dependencies to setup script.

--- setup.sh
+++ setup.sh
@@ -22,3 +22,6 @@ mv en.openfoodfacts.org.products.csv datasets/
 wget http://openrecipes.s3.amazonaws.com/recipeitems-latest.json.gz
 gunzip recipeitems-latest.json.gz
 mv recipeitems-latest.json datasets/
+
+# Get javascript dependencies.
+npm install
